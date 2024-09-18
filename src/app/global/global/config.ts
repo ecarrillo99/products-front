1bfa5803eff136dd32ebd8681de0f401e8e899a2
ecarrillo99 <74939517+ecarrillo99@users.noreply.github.com>
2024-09-18 16:55:22 -0500

    Creación de estructura de directorios y archivos principales

---
+++ src/app/global/global/config.ts
@@ -0,0 +1,10 @@
+export class Config {
+    // Url de servicios
+    static get URL_SERVICES(): string {
+        return "http://localhost:5044/api";
+    }
+
+    static get PRODUCTS(): string {
+        return "/Products/";
+    }
+}

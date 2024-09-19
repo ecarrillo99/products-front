cd544133e99b46ef48ddcbfb55d411bc7d6a28aa
ecarrillo99 <74939517+ecarrillo99@users.noreply.github.com>
2024-09-19 00:18:31 -0500

    Mejorar servicios

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -24,17 +24,6 @@ export class AppMenuComponent implements OnInit {
                         routerLink: ['/inventario']
                     },
                 ]
-            },
-            {
-                label: 'Get Started',
-                items: [
-                    {
-                        label: 'Documentation', icon: 'pi pi-fw pi-question', routerLink: ['/documentation']
-                    },
-                    {
-                        label: 'View Source', icon: 'pi pi-fw pi-search', url: ['https://github.com/primefaces/sakai-ng'], target: '_blank'
-                    }
-                ]
             }
         ];
     }
